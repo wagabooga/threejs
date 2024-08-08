d067fdcda97cb295f080ed6f1421f987908f023b
wagabooga <mattyhutwo@gmail.com>
2024-08-08 13:28:41 -0700

    perspective camera and orthographic camera examples

--- src/main.ts
+++ src/main.ts
@@ -1,98 +1,140 @@
+/**
+ * PRESPECTIVE CAMERA
+ */
+// import './style.css'
+// import * as THREE from 'three'
+// import Stats from 'three/addons/libs/stats.module.js'
+// import { GUI } from 'dat.gui'
+
+// const scene = new THREE.Scene()
+// scene.add(new THREE.GridHelper())
+
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+// camera.position.set(0, 2, 3)
+// camera.lookAt(0, 0.5, 0)
+
+// const renderer = new THREE.WebGLRenderer()
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild(renderer.domElement)
+
+// // important to keep the correct aspect ratio for window resizing
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// })
+
+// const geometry = new THREE.BoxGeometry()
+// const material = new THREE.MeshNormalMaterial({ wireframe: true })
+
+// const cube = new THREE.Mesh(geometry, material)
+// cube.position.y = 0.5
+// scene.add(cube)
+
+// const stats = new Stats()
+// document.body.appendChild(stats.dom)
+
+// const gui = new GUI()
+
+// const cameraFolder = gui.addFolder('Camera')
+// cameraFolder.add(camera.position, 'x', -10, 10)
+// cameraFolder.add(camera.position, 'y', -10, 10)
+// cameraFolder.add(camera.position, 'z', -10, 10)
+// cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
+//   camera.updateProjectionMatrix()
+// })
+// cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
+//   camera.updateProjectionMatrix()
+// })
+// cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
+//   camera.updateProjectionMatrix()
+// })
+// cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
+//   camera.updateProjectionMatrix()
+// })
+// cameraFolder.open()
+
+// function animate() {
+//   requestAnimationFrame(animate)
+
+//   // camera.lookAt(0, 0.5, 0)
+
+//   renderer.render(scene, camera)
+
+//   stats.update()
+// }
+
+// animate()
+
+
+/**
+ * ORTHOGRAPHIC CAMERA
+ */
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'dat.gui';
+import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
-// color
-const sceneA = new THREE.Scene()
-sceneA.background = new THREE.Color(0x123456)
+const scene = new THREE.Scene()
+scene.add(new THREE.GridHelper())
 
-// texture
-const sceneB = new THREE.Scene()
-sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-
-// skybox
-const sceneC = new THREE.Scene()
-sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-
-// additional features
-//sceneC.backgroundBlurriness = 0.5
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+const camera = new THREE.OrthographicCamera(-4, 4, 4, -4, -5, 10)
+camera.position.set(1, 1, 1)
+camera.lookAt(0, 0.5, 0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
+  //camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
-
-new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-// if you have multiple scenes, you have to manage their objects independently, you cant just share the same object across
-sceneA.add(cube)
-// sceneC.add(cube)
-
+cube.position.y = 0.5
+scene.add(cube)
 
 const stats = new Stats()
-// adds stats canvas to dom
 document.body.appendChild(stats.dom)
 
-let activeScene = sceneA
-const setScene = {
-  sceneA: () => {
-    activeScene = sceneA
-  },
-  sceneB: () => {
-    activeScene = sceneB
-  },
-  sceneC: () => {
-    activeScene = sceneC
-  },
-}
-
-/**
- * See other gui's @ Useful Links
- * https://sbcode.net/threejs/dat-gui-module/
- */
 const gui = new GUI()
-gui.add(setScene, 'sceneA').name('Scene A')
-gui.add(setScene, 'sceneB').name('Scene B')
-gui.add(setScene, 'sceneC').name('Scene C')
 
-// const cubeFolder = gui.addFolder('Cube')
-// // GUI.add takes in a Eueler angle (radian) so we use Math.PI*2
-// cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-// cubeFolder.open()
-
-// const cameraFolder = gui.addFolder('Camera')
-// cameraFolder.add(camera.position, 'z', 0, 20)
-// cameraFolder.open()
+const cameraFolder = gui.addFolder('Camera')
+// cameraFolder.add(camera.position, 'x', -10, 10)
+// cameraFolder.add(camera.position, 'y', -10, 10)
+// cameraFolder.add(camera.position, 'z', -10, 10)
+cameraFolder.add(camera, 'left', -10, 0).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'right', 0, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'top', 0, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'bottom', -10, 0).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'near', -5, 5).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'far', 0, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
 
-  /**
-   * can use stats to measure only certain chunk of code
-   *   stats.begin();
-   *   cube.rotation.x += 0.01
-   *   cube.rotation.y += 0.01
-   *   stats.end();
-   */
+  //camera.lookAt(0, 0.5, 0)
 
+  renderer.render(scene, camera)
 
-  renderer.render(activeScene, camera)
   stats.update()
 }
 

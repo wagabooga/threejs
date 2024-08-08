588db099d9d6eae55180666999a34eed0755c28f
wagabooga <mattyhutwo@gmail.com>
2024-08-08 13:12:30 -0700

    introduction to scenes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'dat.gui';
 
-const scene = new THREE.Scene()
+// color
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456)
+
+// texture
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+// skybox
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+
+// additional features
+//sceneC.backgroundBlurriness = 0.5
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
@@ -25,28 +38,47 @@ const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+// if you have multiple scenes, you have to manage their objects independently, you cant just share the same object across
+sceneA.add(cube)
+// sceneC.add(cube)
+
 
 const stats = new Stats()
 // adds stats canvas to dom
 document.body.appendChild(stats.dom)
+
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
 
 /**
  * See other gui's @ Useful Links
  * https://sbcode.net/threejs/dat-gui-module/
  */
 const gui = new GUI()
+gui.add(setScene, 'sceneA').name('Scene A')
+gui.add(setScene, 'sceneB').name('Scene B')
+gui.add(setScene, 'sceneC').name('Scene C')
 
-const cubeFolder = gui.addFolder('Cube')
-// GUI.add takes in a Eueler angle (radian) so we use Math.PI*2
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
+// const cubeFolder = gui.addFolder('Cube')
+// // GUI.add takes in a Eueler angle (radian) so we use Math.PI*2
+// cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+// cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+// cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+// cubeFolder.open()
 
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 20)
-cameraFolder.open()
+// const cameraFolder = gui.addFolder('Camera')
+// cameraFolder.add(camera.position, 'z', 0, 20)
+// cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
@@ -60,7 +92,7 @@ function animate() {
    */
 
 
-  renderer.render(scene, camera)
+  renderer.render(activeScene, camera)
   stats.update()
 }
 
